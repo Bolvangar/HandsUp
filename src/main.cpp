816e72d3e31ecc06e1679a3377de37c981ebe728
Jamjerri <jerri_8.15pm@hotmail.co.uk>
2015-04-27 02:27:36 +0100

    Attempt

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include "testApp.h"
 #include "ofMain.h"
 #include "ofAppGlutWindow.h"
@@ -7,7 +6,7 @@
 int main( ){
 
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 640*2,480*2, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(&window, 640*2,500*2, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
